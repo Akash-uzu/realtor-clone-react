fcc9731d2cca9622103d08aea674439deaa2d0a5
Akash E <idarkmatter98@gmail.com>
2022-12-10 21:17:59 +0530

    created password reset,forgot password,signup pages

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Profile from './pages/Profile';
-import Home from './pages/Home';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
-import ForgotPassword from './pages/ForgotPassword';
-import Offers from './pages/Offers';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Profile from "./pages/Profile";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
+import Header from "./components/Header";
+import Error from "./pages/Error";
 function App() {
   return (
     <>
       <Router>
+        <Header />
         <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/profile" element={<Profile/>}/>
-            <Route path="/sign-in" element={<SignIn/>}/>
-            <Route path="/sign-up" element={<SignUp/>}/>
-            <Route path="/forgot-password" element={<ForgotPassword/>}/>
-            <Route path="/offers" element={<Offers/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="*" element={<Error/>}/>
         </Routes>
       </Router>
     </>
---
+++ src/pages/Error.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Error = () => {
+  return (
+    <h1 className='flex justify-center font-normal py-20 text-2xl'>
+        Sorry, this page isn't available.
+    </h1>
+  )
+}
+
+export default Error
